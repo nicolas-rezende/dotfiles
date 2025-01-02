5174f138ca02dbd5cb58b3e22120dca0d3a5590e
Nicolas Rezende <git@nicolasrezende.com>
2025-01-02 20:54:35 -0300

    wip: nix-darwin

--- flake.nix
+++ flake.nix
@@ -3,12 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }:
+    {
+      nixpkgs,
+      nix-darwin,
+      home-manager,
+      ...
+    }:
     {
       nixosConfigurations = {
         nixos-vmware = nixpkgs.lib.nixosSystem {
@@ -20,6 +27,20 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.nicolas = import ./hosts/nixos-vmware/home.nix;
+            }
+          ];
+        };
+      };
+
+      darwinConfigurations = {
+        macbook = nix-darwin.lib.darwinSystem {
+          modules = [
+            ./hosts/macbook/configuration.nix
+            home-manager.darwinModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.nicolas = import ./hosts/macbook/home.nix;
             }
           ];
         };
---
+++ hosts/macbook/home.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+{
+  imports = [
+    ../../home-manager/common-packages.nix
+    ../../home-manager/common-variables.nix
+    ../../home-manager/fish.nix
+    ../../home-manager/git.nix
+    ../../home-manager/lazygit.nix
+  ];
+
+  home.username = "nicolas";
+  home.homeDirectory = "/home/nicolas";
+
+  xdg.enable = true;
+
+  home.file = {
+    "/home/nicolas/.npmrc".source = ../../dotfiles/npm/.npmrc;
+    # "/home/nicolas/.local/share/code-server/User/settings.json".source =
+    #   ../../dotfiles/vscode/settings.json;
+    # "/home/nicolas/.vscode-extensions.json".source = ../../dotfiles/vscode/extensions.json;
+  };
+
+  # home.packages = with pkgs; [
+  #   # Sync Visual Studio Code Extensions script, this line imports the scripts and adds it to the PATH
+  #   (writeShellScriptBin "se" (builtins.readFile ./scripts/sync-code-server-extensions.sh))
+  # ];
+
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
